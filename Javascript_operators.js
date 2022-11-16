11bca0e60c636f7146fb4b14eb35ed7b0a0bbb02
Manish Sagar <83937898+Manish-Sagar@users.noreply.github.com>
2022-11-16 21:54:36 +0530

    Day - 1

---
+++ Javascript_operators.js
@@ -0,0 +1,71 @@
+// 1. Assignment Operators
+
+var num1 = 10;
+var num2 = 30;
+var n;
+var k = null;
+var string1 = "Manish";
+var string2 = "Sagar";
+console.log(num1+=10) //[-=,*=,/=,**=,%=]
+
+// 2. Arithmetic Operators
+
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1**num2);
+console.log(num1%num2);
+console.log(num1++); 
+console.log(num2--);
+
+// 3. Logical Operators
+
+console.log("operators",num1 && num2);
+console.log(num1 || num2);
+console.log(string1 && string2);
+console.log(string2 && string1);
+console.log(string1 || string2);
+console.log(string2 || string1);
+console.log(!num1);
+// 4. Comparison Operators
+
+console.log(num1>num2);
+console.log(num1>=num2);
+console.log(num1<=num2);
+console.log(num1<num2);
+console.log(num1==num2);
+console.log(num1===num2);
+console.log(num1!=num2);
+
+// 5. Type operator - typeof(), instanceof()
+
+console.log(typeof(num1));
+console.log(typeof(num2));
+console.log(typeof(string1));
+console.log(typeof(n));
+console.log(typeof(k));
+console.log(typeof(k));
+
+// 6. Bitwise Operator
+console.log(num1 & num2);
+console.log(num1 | num2);
+console.log(~num2);
+console.log(~num1);
+console.log(num1^num2);
+console.log(num2^num1);
+console.log(num1<<num2);
+console.log(num1>>num2);
+console.log(num1>>>num2);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javascript_datatypes.js
@@ -0,0 +1,48 @@
+// 1. Number
+var number1 = 10;
+
+// 2. String 
+var string1 = "jsr"; // double quotes
+var string2 = 'newjsr'; //single qoutes
+var string3 = `newnewjsr`; //backticks
+
+// 3. Boolean
+var bool1 = true;
+var bool2 = false;
+
+// 4. Undefined 
+var undefined1;
+// console.log(typeof(undefined1))  // --> returns undefined
+
+// 5. Null 
+var null1 = null;
+
+// 6. Symbol
+
+var obj2 = {
+    name: "manish",
+    age: 19,
+    city: "Hyderabad",
+    gender: 'M',
+    [designaton]:"student",
+}
+var designaton = Symbol("student");
+console.log(obj2);
+
+
+// Non Primitive data types
+// Objects 
+
+var obj1 = {
+    name : "Manish Sagar",
+    rollno : "20951A0582",
+    age : 19,
+    year : 3,
+    sem : 1,
+    sec : 'A'
+}
+
+// Arrays
+
+var array1 = [1,2,3,4,5,6];
+
